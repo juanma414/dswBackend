649cc956ad90b63c570f5df7b0d8eb631786b3db
Lucy <test@test.com>
2024-05-21 16:35:42 -0300

    Terminación del CRUD

--- dist/app.js
+++ dist/app.js
@@ -6,6 +6,20 @@ app.use(express.json()); //--> Solo funciona con Json, con otro tipo no anda
 const users = [
     new User("Pepe No", "Grillo Cambio 2", ["Programador", "PM", "Tester"], "pepe_grillo@test.com", "000123"),
 ];
+//El tipo de Request, Response son de express, lo visualizamos en el import del principio tambien
+//Funciona como un middleware
+function sanitizaUserInput(req, res, next) {
+    req.body.sanitizaInput = {
+        name: req.body.name,
+        lastName: req.body.lastName,
+        rol: req.body.rol,
+        email: req.body.email,
+        id: req.body.id,
+    };
+    //En este lugar se hacen el resto de las validaciones como el tipo, datos maliciosos, no hacer operaciones extrañas,etc.
+    //Si no ponemos next, se queda esperando la respuesta o la resolución a la Request
+    next();
+}
 //Con el user.http podemos llamar a la URI directamente dentro del VS
 app.get("/api/users", (req, res) => {
     res.json(users);
@@ -14,35 +28,44 @@ app.get("/api/user/:id", (req, res) => {
     const user = users.find((user) => user.id === req.params.id);
     if (!user) {
         //Seteamos el valor que devuelve de status y mandamos mensaje de error
-        res.status(404).send({ message: "User no encontrado" });
+        return res.status(404).send({ message: "User no encontrado" });
     }
     res.json(user);
 });
 //User --> request --> express ~~> middleware que forma req.body --> app.post(req.body) -->response -->  User
 //El middleware lo podemos poner en la ruta despues del path como "middleware" o en general al principio.
-app.post("/api/user", (req, res) => {
-    const { name, lastName, rol, email, id } = req.body;
+app.post("/api/user", sanitizaUserInput, (req, res) => {
+    const input = req.body.sanitizaInput;
     //Con la info obtenida creamos el nuevo User
-    const user = new User(name, lastName, rol, email, id);
+    const user = new User(input.name, input.lastName, input.rol, input.email, input.id);
     users.push(user);
     res.status(201).send({ message: "User creado correctamnte", data: user });
 });
-app.put("/api/user/:id", (req, res) => {
+app.put("/api/user/:id", sanitizaUserInput, (req, res) => {
     //Encontrar la
     const userId = users.findIndex((user) => user.id === req.params.id);
     if (userId === -1) {
         //Seteamos el valor que devuelve de status y mandamos mensaje de error
-        res.status(404).send({ message: "User no encontrado" });
+        return res.status(404).send({ message: "User no encontrado" });
     }
-    const input = {
-        name: req.body.name,
-        lastName: req.body.lastName,
-        rol: req.body.rol,
-        email: req.body.email,
-        id: req.body.id,
-    };
-    users[userId] = { ...users[userId], ...input };
+    users[userId] = { ...users[userId], ...req.body.sanitizaInput };
     res.status(200).send({ message: "Modificaciones realizadas", data: users });
+});
+app.delete("/api/user/:id", (req, res) => {
+    const userIdx = users.findIndex((user) => user.id === req.params.id);
+    //El manejo de los status son diferentes según si es que no esta en DB o es porque otro User lo borro
+    if (userIdx === -1) {
+        return res.status(404).send({ message: "User no encontrado" });
+    }
+    users.splice(userIdx, 1);
+    res
+        .status(200)
+        .send({ message: "Usuario borrado correctamente", data: users });
+});
+//Mensaje que se emite en caso de que la URI que se llama no exista y no llame a ningún método de los anteriores
+//Se hace asi, porque sino devuelve un HTML y no una respuesta de API 
+app.use((_, res) => {
+    return res.status(404).send({ message: "Ruta no encontrada" });
 });
 //Sabemos si esta corriendo el Servidor que estamos usando
 app.listen(3000, () => {
--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
 import { User } from "./user.js"; //Por ahora se queda asi, despues se cambia por la DB
 
 const app = express();
@@ -16,41 +16,10 @@ const users = [
   ),
 ];
 
-//Con el user.http podemos llamar a la URI directamente dentro del VS
-app.get("/api/users", (req, res) => {
-  res.json(users);
-});
-
-app.get("/api/user/:id", (req, res) => {
-  const user = users.find((user) => user.id === req.params.id);
-  if (!user) {
-    //Seteamos el valor que devuelve de status y mandamos mensaje de error
-    res.status(404).send({ message: "User no encontrado" });
-  }
-  res.json(user);
-});
-
-//User --> request --> express ~~> middleware que forma req.body --> app.post(req.body) -->response -->  User
-//El middleware lo podemos poner en la ruta despues del path como "middleware" o en general al principio.
-app.post("/api/user", (req, res) => {
-  const { name, lastName, rol, email, id } = req.body;
-
-  //Con la info obtenida creamos el nuevo User
-  const user = new User(name, lastName, rol, email, id);
-  users.push(user);
-  res.status(201).send({ message: "User creado correctamnte", data: user });
-});
-
-app.put("/api/user/:id", (req, res) => {
-  //Encontrar la
-  const userId = users.findIndex((user) => user.id === req.params.id);
-
-  if (userId === -1) {
-    //Seteamos el valor que devuelve de status y mandamos mensaje de error
-    res.status(404).send({ message: "User no encontrado" });
-  }
-
-  const input = {
+//El tipo de Request, Response son de express, lo visualizamos en el import del principio tambien
+//Funciona como un middleware
+function sanitizaUserInput(req: Request, res: Response, next: NextFunction) {
+  req.body.sanitizaInput = {
     name: req.body.name,
     lastName: req.body.lastName,
     rol: req.body.rol,
@@ -58,9 +27,77 @@ app.put("/api/user/:id", (req, res) => {
     id: req.body.id,
   };
 
-  users[userId] = { ...users[userId], ...input };
+  //En este lugar se hacen el resto de las validaciones como el tipo, datos maliciosos, no hacer operaciones extrañas,etc.
+
+  //Si no ponemos next, se queda esperando la respuesta o la resolución a la Request
+  next();
+}
+
+//Con el user.http podemos llamar a la URI directamente dentro del VS
+app.get("/api/users", (req, res) => {
+  res.json(users);
+});
+
+app.get("/api/user/:id", (req, res) => {
+  const user = users.find((user) => user.id === req.params.id);
+
+  if (!user) {
+    //Seteamos el valor que devuelve de status y mandamos mensaje de error
+    return res.status(404).send({ message: "User no encontrado" });
+  }
+
+  res.json(user);
+});
+
+//User --> request --> express ~~> middleware que forma req.body --> app.post(req.body) -->response -->  User
+//El middleware lo podemos poner en la ruta despues del path como "middleware" o en general al principio.
+app.post("/api/user", sanitizaUserInput, (req, res) => {
+  const input = req.body.sanitizaInput;
+
+  //Con la info obtenida creamos el nuevo User
+  const user = new User(
+    input.name,
+    input.lastName,
+    input.rol,
+    input.email,
+    input.id
+  );
+  users.push(user);
+  res.status(201).send({ message: "User creado correctamnte", data: user });
+});
+
+app.put("/api/user/:id", sanitizaUserInput, (req, res) => {
+  //Encontrar la
+  const userId = users.findIndex((user) => user.id === req.params.id);
+
+  if (userId === -1) {
+    //Seteamos el valor que devuelve de status y mandamos mensaje de error
+    return res.status(404).send({ message: "User no encontrado" });
+  }
+
+  users[userId] = { ...users[userId], ...req.body.sanitizaInput };
 
   res.status(200).send({ message: "Modificaciones realizadas", data: users });
+});
+
+app.delete("/api/user/:id", (req, res) => {
+  const userIdx = users.findIndex((user) => user.id === req.params.id);
+
+  //El manejo de los status son diferentes según si es que no esta en DB o es porque otro User lo borro
+  if (userIdx === -1) {
+    return res.status(404).send({ message: "User no encontrado" });
+  }
+
+  users.splice(userIdx, 1);
+  res
+    .status(200)
+    .send({ message: "Usuario borrado correctamente", data: users });
+});
+
+//Mensaje que se emite en caso de que la URI que se llama no exista y no llame a ningún método de los anteriores
+//Se hace asi, porque sino devuelve un HTML y no una respuesta de API 
+app.use((_, res) => {
+  return res.status(404).send({ message: "Ruta no encontrada" });
 });
 
 //Sabemos si esta corriendo el Servidor que estamos usando
